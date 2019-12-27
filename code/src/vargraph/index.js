d2ed04a9f7e9712899258532c9065f52523422d6
Parsus77 <julius.hohlfeld@gmail.com>
2019-12-27 21:12:53 +0100

    VG-16: Load & Save Graph

--- code/src/vargraph/index.js
+++ code/src/vargraph/index.js
@@ -70,12 +70,75 @@ export function createNode(name) {
 }
 
 export function SaveMe(){
-  let elements={
-    nodes: this.savedCyInstance.elements().nodes().jsons(),
-    edges: this.savedCyInstance.elements().edges().jsons()
+  const elements={
+    nodes: cy.elements("node"),
+    edges: cy.elements("edge")
   }
-  alert(elements.nodes + " " +elements.edges);
+  var Output ='';
+  for (let i=0; i<elements.nodes.length;i++){
+    Output += elements.nodes[i].data('id') +" ";
+  }
+  alert(Array.isArray(elements.edges))
+  var String2=''
+  if (Array.isArray(elements.edges)){
+    for (let i=0; i<elements.edges.length;i++){
+      String2 += elements.edges[i].data('id') +" ";
+    }
+  }
+  else String2 = elements.edges.data('id');
+  alert(Output + " " + String2);
+  return elements;
+}
+
+export function Load(graph){
+  cy = cytoscape({
+    container: document.getElementById('cy'),
+
+    elements: [ // list of graph elements to start with
+        {
+        }
+    ],
+
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle'
+        }
+      }
+      // eslint-disable-next-line indent
+    ],
+
+    layout: {
+      name: 'grid',
+      rows: 1
+    }
+  })
+
+  for (let element of graph.nodes){
+    cy.add({
+      data: {id: element.data('id')},
+      position: {x: element.position('x'), y: element.position('y')}
+    });
+  }
+
+  for (let element of graph.edges){
+    cy.add({
+      data: {id: element.data('id'), source: element.data('source'), target: element.data('target')}
+    })
+  }
 }
 
 
-export default {run, createNode, toString, SaveMe}
+export default {run, createNode, toString, SaveMe, Load}
