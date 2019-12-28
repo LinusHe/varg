0c2e575170a49742986cfe7a3327c2613c138837
LinusHe <linus.herterich@gmail.com>
2019-12-28 19:44:14 +0100

    [VG-48] Improved UI

--- code/src/components/newGraph.vue
+++ code/src/components/newGraph.vue
@@ -1,28 +1,38 @@
 <template>
   <v-list-item three-line class="newgraph-container">
     <v-list-item-content>
-      <v-list-item-title align="left" class="newgraph-headline mb-1">Neues Produkt</v-list-item-title>
-      <p class="mt-4 mb-4">
+      <v-list-item-title
+        align="center"
+        class="newgraph-headline mb-1 font-weight-black"
+      >Neues Produkt</v-list-item-title>
+      <p class="mt-4 mb-8 font-weight-light" align="center">
         um einen neuen VARG-Variantengraph zu erstellen,
         musst du zunächst die Produkt-Ausgangsdaten eingeben:
       </p>
+
+      <v-col sm="7">
+        <v-text-field
+          id="prodname"
+          label="Produktname"
+          counter="25"
+          error-message="Produktname ist zu lang"
+        ></v-text-field>
+      </v-col>
+      <v-col sm="5">
+        <v-text-field id="prodquantity" label="Stückzahl" ></v-text-field>
+      </v-col>
+
+      <v-col sm="7">
+        <v-text-field id="prodfirststate" label="Name des ersten Zustands"></v-text-field>
+      </v-col>
+      <v-col sm="5">
+        <router-link to="/graph" tag="button" class="btn-creategraph-link" block>
+          <v-btn block large color="primary" class="btn-creategraph">Starten</v-btn>
+        </router-link>
+      </v-col>
+
       <v-row>
-        <v-col sm="7">
-          <v-text-field id="prodname" placeholder="Produktname" outlined></v-text-field>
-        </v-col>
-        <v-col sm="5">
-          <v-text-field id="prodquantity" placeholder="Stückzahl" outlined></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col sm="7">
-          <v-text-field id="prodfirststate" placeholder="Name des ersten Zustands" outlined></v-text-field>
-        </v-col>
-        <v-col sm="5">
-          <router-link to="menu" tag="button">
-            <v-btn block large color="primary">Starten</v-btn>
-          </router-link>
-        </v-col>
+        <a class="additional-state"><v-icon>mdi-plus</v-icon> weiteren Erstzustand anlegen</a>
       </v-row>
     </v-list-item-content>
   </v-list-item>
