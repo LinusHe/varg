fa83d9c3e11c3efa007fe61cf0db6b9bdeb1db09
Linus Herterich <l.herterich@confirado.de>
2020-01-02 16:09:43 +0100

    [VG-49] Layout Conrtols-Component

--- code/src/components/Controls.vue
+++ code/src/components/Controls.vue
@@ -1,71 +1,69 @@
 <template>
-  <div class="button-container">
-<v-list>
-    <v-row align="center">
-    <v-btn @click="neuerGraph" class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-plus</v-icon>
-    </v-btn>
-    </v-row>
+  <div class="controls">
+    <v-card class="button-card">
+      <v-row align="center">
+        <v-btn @click="neuerGraph" fab dark small depressed color="primary">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-    <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-database-export</v-icon>
-    </v-btn>
-    </v-row>
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-database-export</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-    <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-content-save</v-icon>
-    </v-btn>
-    </v-row>
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-content-save</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-    <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-open-in-app</v-icon>
-    </v-btn>
-    </v-row>
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-open-in-app</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-    <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-image</v-icon>
-    </v-btn>
-    </v-row>
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-image</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-    <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-settings</v-icon>
-    </v-btn>
-    </v-row>
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-settings</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row align="center">
-      <v-btn class="ma-auto" fab dark small color="primary">
-      <v-icon dark>mdi-logout</v-icon>
-    </v-btn>
-    </v-row>
-</v-list>
-
+      <v-row align="center">
+        <v-btn fab dark small depressed color="primary">
+          <v-icon dark>mdi-logout</v-icon>
+        </v-btn>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
 <script>
-import graph from '../vargraph'
-import BasicData from '@/vargraph/BasicData.js'
+import graph from "../vargraph";
+import BasicData from "@/vargraph/BasicData.js";
 
 export default {
-  name: 'Controls',
-  methods: {   
+  name: "Controls",
+  methods: {
     createNode() {
-      graph.createNode(document.getElementById('nodeName').value)
+      graph.createNode(document.getElementById("nodeName").value);
     },
-    neuerGraph: function(){
-      var Name=prompt('Name: ')
-      var Datum=new Date()
-      if (Name === ""){
-        alert('Fehlender Name')
-      }
-      else alert(Name + " " + Datum)
-      new BasicData(Name, Datum)
+    neuerGraph: function() {
+      var Name = prompt("Name: ");
+      var Datum = new Date();
+      if (Name === "") {
+        alert("Fehlender Name");
+      } else alert(Name + " " + Datum);
+      new BasicData(Name, Datum);
     }
   }
-}
+};
 </script>
--- code/src/views/Graph.vue
+++ code/src/views/Graph.vue
@@ -1,30 +1,31 @@
 <style lang="less"></style>
 
 <template>
-
   <div class="graph-page">
-    <GraphHeader/>
-      <v-row>
-        <v-col sm="1">
-          <Controls />
-        </v-col>
-        <v-col sm="10" >
+    <GraphHeader />    
+    <div class="graph-container dotted-background">
+      <v-container fill-height grid-list-md>
+        <v-layout row wrap align-center>
           <VarGraph />
-        </v-col>
-      </v-row>       
+              <Controls />
+        </v-layout>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Controls from '@/components/Controls.vue'
-import VarGraph from '@/components/VarGraph.vue'
+import Controls from "@/components/Controls.vue";
+import VarGraph from "@/components/VarGraph.vue";
+import GraphHeader from "@/components/GraphHeader.vue";
 
 export default {
-  name: 'graph',
+  name: "graph",
   components: {
+    GraphHeader,
     Controls,
     VarGraph
   }
-}
+};
 </script>
