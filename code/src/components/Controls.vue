a9194bd7a287ea1a8a7346ff87bf2ef95a665414
Linus Herterich <l.herterich@confirado.de>
2020-01-02 18:16:59 +0100

    [VG-49] Added Tooltips to Controls

--- code/src/components/Controls.vue
+++ code/src/components/Controls.vue
@@ -2,45 +2,80 @@
   <div class="controls">
     <v-card class="button-card">
       <v-row align="center">
-        <v-btn @click="neuerGraph" fab dark small depressed color="primary">
-          <v-icon dark>mdi-plus</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn @click="neuerGraph" v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-plus</v-icon>
+            </v-btn>
+          </template>
+          <span>Neuer Graph</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-database-export</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-database-export</v-icon>
+            </v-btn>
+          </template>
+          <span>Graph aus Datenbank laden</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-content-save</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-content-save</v-icon>
+            </v-btn>
+          </template>
+          <span>Speichern</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-open-in-app</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-open-in-app</v-icon>
+            </v-btn>
+          </template>
+          <span>Graph laden</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-image</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-image</v-icon>
+            </v-btn>
+          </template>
+          <span>Als Bild speichern</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-settings</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-settings</v-icon>
+            </v-btn>
+          </template>
+          <span>Einstellungen</span>
+        </v-tooltip>
       </v-row>
 
       <v-row align="center">
-        <v-btn fab dark small depressed color="primary">
-          <v-icon dark>mdi-logout</v-icon>
-        </v-btn>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" fab dark small depressed color="primary">
+              <v-icon dark>mdi-logout</v-icon>
+            </v-btn>
+          </template>
+          <span>Ausloggen</span>
+        </v-tooltip>
       </v-row>
     </v-card>
   </div>
