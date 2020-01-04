2261a1a025bfa5dfe4d50ee389f3c86a71883217
Parsus77 <julius.hohlfeld@gmail.com>
2020-01-04 16:35:37 +0100

    Prudukt to Produkt

--- code/src/components/GraphHeader.vue
+++ code/src/components/GraphHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header-graph-container">
-      <v-list-item-title align="center" class="login-pre-headline mt-6">Prudukt:</v-list-item-title>
+      <v-list-item-title align="center" class="login-pre-headline mt-6">Produkt:</v-list-item-title>
     <v-list-item-title align="center" class="login-headline mb-1">Anzahl:</v-list-item-title>
   </div>
 </template>
