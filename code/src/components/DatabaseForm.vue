ec879c9dc149a415cadb8eaa349671df33d9c2a8
Karkoutli <alaa_aldin.karkoutli@stud.htwk-leipzig.de>
2020-01-04 19:51:17 +0100

    [vg43] Database-Design umsetzen

--- code/src/components/DatabaseForm.vue
+++ code/src/components/DatabaseForm.vue
@@ -7,7 +7,7 @@
          <!--v-btn class = "li" id="r" @click="re(true)"><a href="#Graphen-entfernen"><center> Remove </center></a></!--v-btn>-->
          <v-btn class="li" @click="go()"><a href="#Musterlösungen"><center>Muster</center></a></v-btn>
          <v-btn class="li" @click="zurueck()"><a href="#zurueck"><center> Zurueck </center></a></v-btn>
-         <div class="vr">&nbsp;</div>
+         <div class="vr"></div>
         <div align="left" id="First">
         <img id="0" class="rectangle" src="../assets/img/g.png" @click="selected(0)"/>
         </div>
@@ -26,24 +26,24 @@
          alert("Graph " + index+ " selected!");
      },
          myfunc(){
-           if ( b < 1100) {
+           if ( b < 1100 && t <= 748 && r == false) {
             b = b+z;
             document.getElementById("First").innerHTML+= '<img id="'+i+'"class="rectangle" style ="left:'+b+'px;top:'+t+'px" src="../assets/img/g.png" @click="selected('+i+')" />';
             i++;
           }
-          else if (b >= 1100){
+          else if (b >= 1100 && t <= 748 && r == false){
             b = 130;
             t = t+132;
             document.getElementById("First").innerHTML+= '<img id="'+i+'" class="rectangle" style="left:'+ b +'px;top:'+ t +'px" src="../assets/img/g.png" @click="selected('+i+')" /> ';
             i++;
           }
-          else if (t > 748){
+          else if ((t == 748 || t > 748) && b == 130 && r == false){
           alert("Datnbank ist voll!");
           }
         else if (r == true) {
           alert("Error Musterlösungen");
         }
-         },
+      },
     go(){
       if (r == false){
         document.getElementById("First").innerHTML = '<img id="1" class="rectangle" src="../assets/img/g.png" style="left:130px" @click="selected(1)" />'+'<img id="2" class="rectangle" style="left:260px" src="../assets/img/g.png" @click="selected(2)" />'+'<img id="3" class="rectangle"  style="left:390px" src="../assets/img/g.png" @click="selected(3)" />'+'<img id="4" class="rectangle" style="left:550px" src="../assets/img/g.png" @click="selected(4)" />';
