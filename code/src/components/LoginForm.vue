f731983d4fff290567d8346a96e20ec0b0faff08
Lennart <lennart.buchmann@icloud.com>
2020-01-05 20:29:42 +0100

    login

--- code/src/components/LoginForm.vue
+++ code/src/components/LoginForm.vue
@@ -4,6 +4,7 @@
     <v-list-item-title align="center" class="login-pre-headline mt-5">Variantengraph-Editor</v-list-item-title>
     <v-list-item-title align="center" class="login-headline mb-1">varg</v-list-item-title>
     <v-text-field
+    v-model="input.email"
       class="mt-8"
       id="email"
       label="E-Mail"
@@ -12,16 +13,17 @@
       clearable
     ></v-text-field>
     <v-text-field
-      v-model="password"
+      v-model="input.password"
       :type="show1 ? 'text' : 'password'"
       id="password"
       label="Passwort"
       placeholder="******"
+      
       outlined
     ></v-text-field>
-    <router-link to="menu" tag="button">
-      <v-btn class="login-button" @click="createNode" large color="primary">Login</v-btn>
-    </router-link>
+    <!--<router-link to="menu" tag="button">-->
+      <v-btn class="login-button" @click="login()" large color="primary">Login</v-btn>
+    <!--</router-link>-->
 
     <a align="center" class="font-italic mt-6" color="error">Passwort vergessen?</a>
     <p align="center" class="login-bottom-links mt-10" color="lightgrey">
@@ -35,6 +37,56 @@
 
 <script>
 export default {
-  name: "LoginForm"
-};
+  name: "LoginForm",
+  data() {
+      return {
+        input: {
+          email: '',
+          password: '',
+        },
+        error: '',
+      }
+    },
+    methods: {
+
+      /**
+       * Validates user input and on success redirects to home view.
+       * @var authenticated can be used to verrify if a user has logged in succesfully.
+       */
+      login() {
+        
+        if(this.input.email != "" && this.input.password != "") {
+          if(this.input.email == "VarG" && this.input.password == "2020") {
+            print
+            //this.$emit("authenticated", true);
+            this.$router.replace("/home/menu");
+          } else {
+            this.error = 'Falscher Nutzername oder falsches Passwort'
+          }
+        } else {
+          this.error = 'Leere Eingabe'
+        }
+      },
+
+      /**
+       * Clears previous error messages
+       */
+      clearError() {
+        this.error = ''
+      },
+    }
+}
+
+
+
+;
 </script>
+
+<!--"createNode"
+
+@click="login()"
+
+
+
+
+<button [routerLink]="( ? ['/menu'] : []"></button> -->
