678347f1fa5ec642e9fb4cb0588a8509cfe6d828
Parsus77 <julius.hohlfeld@gmail.com>
2020-01-16 23:47:38 +0100

    VG-80: SaveDialog

---
+++ code/src/components/SaveMenu.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-dialog v-model="dialog" max-width="350">
+        <v-card>
+            <v-card-title class="headline">Speichern</v-card-title>
+            <v-card-text>
+                Legen sie einen Namen für den Graphen in der Datenbank fest.
+                <v-row>
+                    <v-text-field
+                            class="mt-2"
+                            id="DatabaseName"
+                            label="Datenbankname"
+                            v-model="DataBaseName"
+                            outlined
+                            hide-details
+                    >
+                    </v-text-field>
+                    <v-card-actions>
+                        <v-col sm="6">
+                            <v-btn color="success" outlined @click="save">Speichern</v-btn>
+                        </v-col>
+                        <v-spacer></v-spacer>
+                        <v-col sm="6">
+                            <v-btn color="error" outlined @click="clearFields">Abbrechen</v-btn>
+                        </v-col>
+                    </v-card-actions>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    name: 'SaveMenu',
+    data()  {
+        return{
+            dialog: false,
+            DataBaseName: "",
+        };
+    },
+    methods:    {
+        setdialog(boolean) {
+            this.dialog=boolean
+        },
+        setLabel(value) {
+            this.DataBaseName=""
+            document.getElementById('DatabaseName').label=value
+        },
+        save()  {
+            if (this.DataBaseName!= "" && this.DataBaseName!= null){
+                this.$emit('onSaveConfirm',this.DataBaseName)
+            }
+            else this.$emit('onSaveConfirm',null)
+        },
+        clearFields()   {
+            this.dialog=false
+            this.DataBaseName=""
+        }
+    }
+}
+</script>
