c1bcd7e91b8c67a641fc6adc77fa2c33f6865daa
Parsus77 <julius.hohlfeld@gmail.com>
2020-03-03 19:59:09 +0100

    VG-58: Database test

--- code/src/components/graph/elements/Toolbar.vue
+++ code/src/components/graph/elements/Toolbar.vue
@@ -15,7 +15,7 @@
       <v-row align="center">
         <v-tooltip right>
           <template v-slot:activator="{ on }">
-            <v-btn @click="datenbank()" v-on="on" fab dark small depressed color="primary">
+            <v-btn @click="datenbank()" id="database-btn" v-on="on" fab dark small depressed color="primary">
               <v-icon dark>mdi-database-export</v-icon>
             </v-btn>
           </template>
