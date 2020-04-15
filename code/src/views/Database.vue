1d2d8f2e79f242fba0ea821d0a7205dd27758aad
Jonas Gwozdz <jonas.gwozdz@stud.htwk-leipzig.de>
2020-04-15 12:12:48 +0200

    VG-90 Fortschritt

--- code/src/views/Database.vue
+++ code/src/views/Database.vue
@@ -2,8 +2,25 @@
 
 <template>
   <div class="database-page">
-      <img alt="htwk logo" id="htwk-logo" src="../assets/img/htwk.svg">
+      <HtwkLogoHeader></HtwkLogoHeader>
       <datenbank/>
+      <div class="card-container dotted-background">
+      <v-container fill-height grid-list-md>
+        <v-layout row wrap align-center>
+          <v-card class="login-card mx-auto mb-4">
+            <transition
+              name="fade"
+              mode="out-in"
+              @beforeLeave="beforeLeave"
+              @enter="enter"
+              @afterEnter="afterEnter"
+            >
+              <router-view></router-view>
+            </transition>
+          </v-card>
+        </v-layout>
+      </v-container>
+    </div>
       <p align="center" class="bottom-links" color="lightgrey" height="100%">
       <a>Backend</a> |
       <a>Impressum</a> |
@@ -14,13 +31,15 @@
 
 <script>
 // @ is an alias to /src
-import datenbank from "@/components/database/DatabaseForm.vue"
 
+import datenbank from "@/components/database/DatabaseForm.vue"
+import HtwkLogoHeader from "@/components/basic/HtwkLogoHeader";
 export default {
   name: 'database',
   components: {
 
-    datenbank
+    datenbank,
+    HtwkLogoHeader
   }
   }
 
