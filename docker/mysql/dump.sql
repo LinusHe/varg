18c6258a092112afcdb9579cc7b9a94afbdfb316
Erik Heldt <erik.heldt@stud.htwk-leipzig.de>
2020-04-29 21:23:16 +0200

    minor fix for sql examples

--- docker/mysql/dump.sql
+++ docker/mysql/dump.sql
@@ -1,16 +1,16 @@
-Create Table userreg (
+CREATE TABLE userreg (
     userid VARCHAR(20),
-    auth Varchar(20),
+    auth VARCHAR(20),
     PRIMARY KEY (userid)
 );
 
-Insert Into userreg Values ("jhohlfel", "admin");
-Insert Into userreg Values ("eheldt", "admin");
+INSERT INTO userreg VALUES ("jhohlfel", "admin");
+INSERT INTO userreg VALUES ("eheldt", "admin");
 
-Create Table Graph (
-    graphname VARCHAR(25) Not Null,
-    prodname VARCHAR(25) Not Null,
+CREATE TABLE graph (
+    graphname VARCHAR(25) NOT Null,
+    prodname VARCHAR(25) NOT Null,
     PRIMARY KEY (graphname)
 );
 
-INSERT INTO userreg Values ("test", "Schrauben");
+INSERT INTO graph VALUES ("sampleGraph", "Schrauben");
