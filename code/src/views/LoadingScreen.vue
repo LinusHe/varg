beca866d5650ed9cb166158c0c999c0887ffa600
Lennart <lennart.buchmann@icloud.com>
2020-05-06 21:17:43 +0200

    first try at implementation of successfull redirect after page reload

--- code/src/views/LoadingScreen.vue
+++ code/src/views/LoadingScreen.vue
@@ -12,11 +12,13 @@
 export default {
   name: "LoadingScreen",
   mounted: function() {
-    if (Date.now() > (this.$store.getters.getIssuedTime + 3000)) {
+    if (Date.now() > (this.$store.getters.getIssuedTime + 3000000000000)) {
           localStorage.removeItem("store");
           this.$store.commit("logout");
           this.$router.replace("/home/login");
-    } else if (this.$store.getters.getAuth) {
+    } else if (this.$store.getters.getAuth && window.history.length > 1) {
+      this.$router.replace(this.$route.query.redirect);
+    } else if (this.$store.getters.getAuth && window.history.length <= 1) {
       this.$router.replace("/home/menu");
     } else {
       this.$router.replace("/home/login");
@@ -25,3 +27,13 @@ export default {
 
 };
 </script>
+
+
+
+      if (window.history.length > 1 ) {
+
+          //this.$router.push.redirect();
+      } else {
+        this.$router.replace("home/menu");
+      }
+      //alert("ROUUUTING");
