c89de8600ec7b509314ae488f0be71393207e21f
dksoch1 <58476188+dksoch1@users.noreply.github.com>
2020-05-06 12:59:06 +0200

    new algorithm finished
    Neuer Algorithmus funktional, gibt die bestern Pfade (Anzahl abhängig von Eingabe) in entsprechender Reihenfolge aus (tests fehlen)

--- code/src/components/graph/controls/OptimizeControls.vue
+++ code/src/components/graph/controls/OptimizeControls.vue
@@ -93,171 +93,284 @@ export default {
     getNodeItemsName() {
       this.itemsName = this.getGraph().getNodeName(this.getGraph());
     },
+    getTotalCost(Edges) {
+      let totalCost = 0
+      for(let i = 0; i < Edges.length; i++) {
+        totalCost += this.getGraph().getTotalCost(Edges[i])
+      }
+      return totalCost      
+    },
+    getTotalTime(Edges) {
+      let totalTime = 0
+      for(let i = 0; i < Edges.length; i++) {
+        totalTime += this.getGraph().getTotalTime(Edges[i])
+      }   
+      return totalTime   
+    },
+    getOptionValue(path) {
+      let value
+      let option = this.optimizingOption;   // false = time, true = cost
+      if (option) {
+        value = this.getTotalCost(path)
+      }
+      else {
+        value = this.getTotalTime(path)
+      }
+      return value
+    },
+    wasAllreadyFound(path, foundPaths) {
+      //checks, if path is in foundPaths
+      let found = false
+      for (let i = 0 ; i < foundPaths.length && found == false; i++) {
+        if (path.length == foundPaths[i].length) {
+          let partFound = true
+          for (let j = 0; j < path.length && j < foundPaths[i].length && partFound; j++) {
+            if (path[j] != foundPaths[i][j]) {
+              partFound = false
+            }
+          }
+          if(partFound == true) {
+            found = true
+          }
+        }
+      }
+      return found
+    },
+    findNextBest(path, sortNode, foundPaths) {
+      let distance = -2
+      let nextPath = []
+      let nextPathPerNode = []
+      let oldCost, newCost
+
+        // check all nodes on best path for the "best better way"   
+      for(let changingNode=0; changingNode < path.length; changingNode++) {
+        let nextSearch = 0
+        
+          //checkNode = next node on the path, starts at the first after startnode
+        let checkNode = this.getGraph().getTarget(path[changingNode])
+        
+          // look for checkNode's Id in sortNodes[x][0], first one found is the currently used path (sortNodes[x][1])
+        while(nextSearch < sortNode.length && sortNode[nextSearch][0] != checkNode) {
+          nextSearch ++
+        }
+        
+        if (nextSearch < sortNode.length) {
+          oldCost = sortNode[nextSearch][2]
+        }
+        else {
+          oldCost = -2
+        }
+        nextSearch++
+          // second one found is the next best
+          // if there is no more, nextSearch == sortNode.length
+        while(nextSearch < sortNode.length && sortNode[nextSearch][0] != checkNode) {
+          nextSearch ++
+        }
+          //if there was a nextbest path to this node
+        if (nextSearch < sortNode.length) {
+            //nextPathPerNode is the nextbest path to this node plus the rest of path
+          nextPathPerNode = this.changePath(sortNode[nextSearch], path)
+          newCost = sortNode[nextSearch][2]
+        }
+        else {
+          newCost = -2
+        }
+
+          //if nextPathPerNode is allready in bestPaths, look for the next x in sortNodes
+        while(this.wasAllreadyFound(nextPathPerNode, foundPaths)) {
+          nextSearch ++
+          while(sortNode[nextSearch][0] != checkNode && nextSearch < sortNode.length) {
+            nextSearch ++
+          }
+          if (nextSearch < sortNode.length) {
+            nextPathPerNode = this.changePath(sortNode[nextSearch], path)
+            newCost = sortNode[nextSearch][2]
+          }
+          else {
+            nextPathPerNode = []
+          }
+        }
+          // check if distance is new best(low) and save id
+          // || distance < 0 to make sure, distance for first node is used
+          // if nextPath did not get an acttual path, nextPath.length != 0 to avoid errors
+          //anpassen?
+        if((distance > newCost-oldCost || distance < 0) && this.wasAllreadyFound(nextPathPerNode, foundPaths) == false && nextPathPerNode.length != 0) {
+            distance = newCost-oldCost
+            nextPath = nextPathPerNode
+        }
+      }
+      return nextPath
+    },
+    isPartOfPath(node, path){
+        //checks if and where node exists on path (as target of an edge in path)
+      let point = -2
+      for (let i = 0; i < path.length; i++) {
+        if(this.getGraph().getTarget(path[i]) == node) {
+          point = i
+        }
+      }
+      return point
+    },
+    isPartOfNodes(node, nodes){
+        //checks if node is in nodes
+      let point = false
+      for (let i = 0; i < nodes.length-1; i++) {
+        if(node == nodes[i]) {
+          point = true
+        }
+      }
+      return point
+    },
+    changePath(newNode, oldPath) {
+        //combines newNode[1] and oldPath
+        //first half: newNode[1]
+        //second half: oldPath starting after the last target in newNode[1]
+      let point = this.isPartOfPath(newNode[0], oldPath)
+                            
+      if(point >= 0) {
+        let tempPath = []
+        for (let temp = 0; temp < newNode[1].length; temp ++) {
+          tempPath[temp] = newNode[1][temp]
+        }
+        let secondPart = newNode[1].length + oldPath.length - (point + 1)
+        for (let temp = newNode[1].length; temp < secondPart; temp ++) {
+          point ++
+          tempPath[temp] = oldPath[point]
+        }
+        oldPath = tempPath
+      }
+      return oldPath
+    },
     optimizing: function() {
       let option = this.optimizingOption;   // false = time, true = cost
+      let nextBestCounter = 4
+      //in neuer gui einfügen:
+      //let nextBestCounter = this.nextBestCounter
       
-      /*
-      if (this.optimizingOption === false) {
-        // True means option costs and false is option time
-        // not implemented yet
-        option = "optionTime";  // false = time, true = cost
-      } else {
-        option = "optionCosts"; //cost
-      }
-      */  
-
         // gets ID's of start-nodes
       let startIDs = []
       for (let i = 0; i < this.startSelect.length; i++) {
         let indexStart = this.itemsName.indexOf(this.startSelect[i]);
         startIDs.push(this.itemsID[indexStart]);
       }
-
         
         // "converts" start-nodes into sort-nodes
         // sort-nodes save NodeID, usedEdges (from start-node) and "needed" costs (from start)
       let sortNodes = [];
       for (let i = 0; i < startIDs.length; i++) {
         let Edges = [];
-
         let optimizeNode = [3]
         optimizeNode [0] = startIDs[i]          //NodeID
         optimizeNode [1] = Edges                //usedEdges
-        optimizeNode [2] = 0                    //cost/time
-        
+        optimizeNode [2] = 0                    //cost/time        
         sortNodes.push(optimizeNode);
       }
 
-        // for each node...
+        //checkedNodes noch nicht funktional, aber nur für performance relevant
+      let checkedNodes = []
+
       for (let i = 0; i < sortNodes.length; i++) {
-          //testline
-        console.log(sortNodes.length)
+          //checkedNodes noch nicht funktional, aber nur für performance relevant
+        if(this.isPartOfNodes(sortNodes[i][0], checkedNodes) == false) {
 
-        let Edges = [];
+          let Edges = [];
+          let optimizeNode = [3]
 
-        let optimizeNode = [3]
+            // nextEdgeCounter = number of reachable nodes
+          let nextEdgeCounter = this.getGraph().countNewEdges(sortNodes[i])
+          let nextNode   
+          
+            // get next reachable nodes in sort-node form
+            // getting nexNodes individually from graph, otherwise problems (somehow)
+          for (let j = 0; j < nextEdgeCounter; j++) {
 
-          // ...get next gettable nodes in sort-node form
+            nextNode = this.getGraph().getNextNode(sortNodes[i], option, j)
 
-        let nextEdgeCounter = this.getGraph().countNewEdges(sortNodes[i])
+            let allreadyFound = 0;
+              // checks if nextNode is allready part of sort-node (checking ID)
+            for (let k = 0; k < sortNodes.length; k++) {
 
-        console.log(nextEdgeCounter)
+                // node was allready found
+                //'allreadyfound' ensures, only top 'nextBestCounter' costs of every node gets saved
+              if (sortNodes[k][0] == nextNode [0]) {
+                allreadyFound ++
 
-        
-        let nextNode   
-      
-          // for each "new found" node...
-          // getting nexNodes individually from grahp, otherwise problems (somehow)
-        for (let j = 0; j < nextEdgeCounter; j++) {
+                  // if new found way has lower cost correct all nodes that had it as part of their way
+                if (sortNodes[k][2] > nextNode [2]) {
+                  if(allreadyFound == 1) {
+                    for (let correct = 0; correct < sortNodes.length; correct++) {
+                      if(nextNode[0] != sortNodes[correct][0]) {
+                        //otherwise would overwrite everything to that node anytime a new better path to a node is found
+                        let tempPath = this.changePath(nextNode, sortNodes[correct][1])
+                      
+                        sortNodes[correct][1] = tempPath
+                        sortNodes[correct][2] = this.getOptionValue(tempPath)
+                      }
+                    } 
+                  }
+                    //insertion-sort-like
+                  let tempCost = sortNodes[k][2]
+                  sortNodes[k][2] = nextNode[2]
+                  nextNode[2] = tempCost
 
-          nextNode = this.getGraph().getNextNode(sortNodes[i], option, j)
-
-          let allreadyFound = 0;
-            // ...check if node is allready part of sort-node (checking ID)
-          for (let k = 0; k < sortNodes.length; k++) {
-              // node was allready found
-              //'allreadyfound' ensures, only top 3 costs of every note gets saved
-            
-            if (sortNodes[k][0] == nextNode [0]) {
-              allreadyFound ++
-
-                // if new found way has lower cost
-              if (sortNodes[k][2] > nextNode [2]) {
-                  // override it
-                let tempCost = sortNodes[k][2]
-                sortNodes[k][2] = nextNode[2]
-                nextNode[2] = tempCost
-
-                let tempPath = sortNodes[k][1]
-                sortNodes[k][1] = nextNode[1]
-                nextNode[1] = tempPath
+                  let tempPath = sortNodes[k][1]
+                  sortNodes[k][1] = nextNode[1]
+                  nextNode[1] = tempPath
+                }
               }
             }       
+            if(allreadyFound < nextBestCounter) {
+              sortNodes.push(nextNode)
+            }
           }
-          
-            // new found node, 3 in case of Top 3 search
-          if(allreadyFound < 3) {
-            sortNodes.push(nextNode)
-            
-          }
-
-          //testline
-          for (let l = 0; l < sortNodes.length; l++) {
-            console.log(sortNodes[l])
-          }
-
+            //checkedNodes noch nicht funktional, aber nur für performance relevant          
+          checkedNodes.push(sortNodes[i][0])
         }
       }
-      
-      
-      //searches for the end-node
+    
+      let bestPaths = []
+      let bestPath = [3]
       let indexEnd = this.itemsName.indexOf(this.endSelect);
       let endID = this.itemsID[indexEnd];
+      
+        //look for the end-node, first one found is the one with lowest cost
       let search = 0
-      while(sortNodes[search][0] != endID) {
+      while(sortNodes[search][0] != endID && search < sortNodes.length) {
         search ++
       }
-      let bestPath = sortNodes[search][1]
-      let bestCost = sortNodes[search][2]
 
-      console.log(sortNodes[search])
-      this.getGraph().markBestEdges(bestPath)
+      bestPaths.push(sortNodes[search][1])
 
-      
-      let distance = 0
-      let distanceID, pointInPath
-      let nextSearch = 0
-
-        // chack all nodes on best path for the "best better way"
-        // for all nodes...
-      
-     
-      for(let i=0; i < bestPath.length; i++) {
-        let checkNode = this.getGraph().getTarget(bestPath[i])
-        
-          // ...old cost....
-        while(sortNodes[nextSearch][0] != checkNode) {
-          nextSearch ++
-        }
-        let oldCost = sortNodes[nextSearch][2]
-
-        nextSearch++
-
-          // ...new cost
-        while(sortNodes[nextSearch][0] != checkNode) {
-          nextSearch ++
-        }
-
-        let newCost = sortNodes[nextSearch][2]
-
-          // check if distance is new best(low) and save id
-        if(distance < oldCost-newCost || i==0) {
-          distance = oldCost - newCost
-          distanceID = checkNode
-          pointInPath = i + 1
-        }
-        
-
+/*    für später, evlt?
+      if(search == sortNodes.length) {
+        console.log("Kein Weg möglich...")
       }
-      
-
-      let nextBestCost = bestCost - distance
-      let nextBestPath = sortNodes[nextSearch][1]
-
-      
-      for(let i=pointInPath.length; i<bestPath.lenght; i++) {
-        nextBestPath.push(bestPath[i])
+      else {
+        bestPaths.push(sortNodes[search][1])
       }
-      
-        
+      */
 
+        //saves the nextbest alternatives in array
+      for(let rank = 1; rank < nextBestCounter; rank++) {
+        let nextRank, nextRankPath
+        let nextRankDistance = -2
+        for (let checks = 0; checks < bestPaths.length; checks++) {
+          nextRankPath = this.findNextBest(bestPaths[checks], sortNodes, bestPaths)
+          if (nextRankPath.length > 0 && (this.getOptionValue(nextRankPath) - this.getOptionValue(bestPaths[0]) < nextRankDistance || nextRankDistance < 0)) {
+            nextRankDistance = this.getOptionValue(nextRankPath) - this.getOptionValue(bestPaths[0])
+            nextRank = nextRankPath
+          }
+        }
+        bestPaths.push(nextRank)
+      }
+    
+      this.getGraph().markBestEdges(bestPaths[0])
 
-      console.log(bestPath)
-      console.log(bestCost)
-
-      console.log(nextBestPath)
-      console.log(nextBestCost)
-      //this.getGraph().markBestEdges(bestPath)
+      for(let ranking = 0; ranking < bestPaths.length; ranking ++) {
+        console.log(bestPaths[ranking])
+        console.log(this.getTotalCost(bestPaths[ranking]))
+        console.log(this.getTotalTime(bestPaths[ranking]))
+      }
 
     }
   }
--- code/src/vargraph/graph/optimizations.js
+++ code/src/vargraph/graph/optimizations.js
@@ -8,76 +8,14 @@ import cyStore from "./cyStore";
 
 
 export default {
-  // findPath(.. ): The method finds the shortest Path between 2 nodes
-  //                with the Dijkstra Algorithm
-  findPath(graphComponent, option, start, end) {
-    // get cytoscape instance
-    //let cy = graphComponent.getCytoGraph();
-
-    var minDistance = 0;
-
-    cyStore.data.cy.elements().removeClass("highlighted");
-    let cy = cyStore.data.cy
-
-    var endNode = "#" + end;
-
-    if (option === "optionCosts") {
-      console.log("costs");
-      var pathToEndCosts;
-      for (let i = 0; i < start.length; i++) {
-        let startNode = "#" + start[i];
-        var dijkstraCosts = cy.elements().dijkstra(startNode, function(edge) {
-          return edge.data("cost") + edge.data("sucost");
-        });
-        if (i === 0) {
-          //saves the shortest distance to a sspecific node(in this case endNode)
-          minDistance = dijkstraCosts.distanceTo(cy.$(endNode));
-          //saves the shortes path to a specific node
-          pathToEndCosts = dijkstraCosts.pathTo(cy.$(endNode));
-        } else if (minDistance > dijkstraCosts.distanceTo(cy.$(endNode))) {
-          minDistance = dijkstraCosts.distanceTo(cy.$(endNode));
-          pathToEndCosts = dijkstraCosts.pathTo(cy.$(endNode));
-        }
-      }
-      pathToEndCosts.addClass("highlighted");
-    }
-
-    if (option === "optionTime") {
-      console.log("time");
-
-      var pathToEndTime;
-      for (let i = 0; i < start.length; i++) {
-        let startNode = "#" + start[i];
-        var dijkstraTime = cy.elements().dijkstra(startNode, function(edge) {
-          return edge.data("time") + edge.data("sutime");
-        });
-        if (i === 0) {
-          minDistance = dijkstraTime.distanceTo(cy.$(endNode));
-          pathToEndTime = dijkstraTime.pathTo(cy.$(endNode));
-        } else if (minDistance > dijkstraTime.distanceTo(cy.$(endNode))) {
-          minDistance = dijkstraTime.distanceTo(cy.$(endNode));
-          pathToEndTime = dijkstraTime.pathTo(cy.$(endNode));
-        }
-      }
-      pathToEndTime.addClass("highlighted");
-    }
-  },
-
-  
   countNewEdges(start) {
     let cy = cyStore.data.cy
-    
     let EdgeCount = 0
-
-      // all outgoing edges from node 'start'
     cy.getElementById(start[0]).outgoers('edge').forEach(element => {
       EdgeCount ++
     })
-
     return EdgeCount
   },  
-  
-  
   
   getNextNode(start, option, count) {
     let cy = cyStore.data.cy
@@ -86,9 +24,6 @@ export default {
       // all outgoing edges from node 'start'
     let nextEdges = cy.getElementById(start[0]).outgoers('edge');
     let newFoundNode = [3];
-
-
-    console.log(1000)
 
       //converts all outgoing edges into sort-node form
     newFoundNode [0] = nextEdges[count].data("target")
@@ -100,25 +35,18 @@ export default {
       newPath.push(start[1][i])
     }
     newPath.push(nextEdges[count].data("id"))
-    newFoundNode [1] = newPath                                          //usedEdges
+    newFoundNode[1] = newPath
 
     if(option) {
-      let newtime = start[2] + nextEdges[count].data("time") * quantity + nextEdges[count].data("sutime")
-      newtime = Math.round(newtime * 100) / 100
-
-      newFoundNode [2] = newtime              
-    }
-    else {
       let newcost = start[2] + nextEdges[count].data("cost") * quantity + nextEdges[count].data("sucost")
       newcost = Math.round(newcost * 100) / 100
-
-      newFoundNode [2] = newcost            
+      newFoundNode [2] = newcost           
     }
-
-      //testline
-    console.log(newFoundNode[2])
-    console.log(nextEdges.length)
-    
+    else {
+      let newtime = start[2] + nextEdges[count].data("time") * quantity + nextEdges[count].data("sutime")
+      newtime = Math.round(newtime * 100) / 100
+      newFoundNode [2] = newtime            
+    }
 
     return newFoundNode
   },
@@ -126,18 +54,30 @@ export default {
   markBestEdges(bestEdgesID) {
     cyStore.data.cy.elements().removeClass("highlighted");
     let cy = cyStore.data.cy
-
     bestEdgesID.forEach(element => {
       cy.getElementById(element).addClass("highlighted")
     })
   },
 
-  getTarget(edge){
+  getTarget(edge) {
     let cy = cyStore.data.cy
-
     let node = cy.getElementById(edge).data("target")
-
     return node
-  }
+  },
 
+  getTotalCost(edge) {
+    let cy = cyStore.data.cy
+    let quantity = cy.data("prodQuant")
+    let partcost = cy.getElementById(edge).data("cost") * quantity + cy.getElementById(edge).data("sucost")
+    partcost = Math.round(partcost * 100) / 100
+    return partcost
+  },
+
+  getTotalTime(edge) {
+    let cy = cyStore.data.cy
+    let quantity = cy.data("prodQuant")
+    let parttime = cy.getElementById(edge).data("time") * quantity + cy.getElementById(edge).data("sutime")
+    parttime = Math.round(parttime * 100) / 100
+    return parttime
+  }
 };
