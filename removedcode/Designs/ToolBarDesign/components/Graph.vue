49e70b2de91507c1e29ef813db930e7ae80ef669
Linus Herterich <l.herterich@confirado.de>
2020-05-07 20:39:38 +0200

    VG-157 Added ToolbarDesign to removedcode

---
+++ removedcode/Designs/ToolBarDesign/components/Graph.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-app id="inspire">
+    <NavigationDrawer ref="navigationDrawer" />
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      flat
+      color="white"
+    >
+      <v-app-bar-nav-icon class="ml-1" @click.stop="toggleDrawer()"/>
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">varg :: Variantengraph-Editor</span>
+      </v-toolbar-title>
+      <!--      <v-text-field-->
+      <!--        flat-->
+      <!--        solo-inverted-->
+      <!--        hide-details-->
+      <!--        prepend-inner-icon="mdi-magnify"-->
+      <!--        label="Search"-->
+      <!--        class="hidden-sm-and-down"-->
+      <!--      />-->
+      <v-card flat light>
+        <GraphHeader ref="graphHeader"/>
+      </v-card>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          />
+        </v-avatar>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        class="fill-height dotted-background"
+      >
+
+        <VarGraph ref="vargraph"/>
+
+        <v-row
+          align="center"
+          justify="center"
+        >
+
+
+          <!-- Controls:  -->
+          <CreateControls ref="createControls"/>
+          <DetailControls ref="detailControls"/>
+          <ZoomControls ref="zoomControls"/>
+
+          <!-- Menus:  -->
+          <NewGraphMenu ref="newGraphMenu"/>
+          <SaveMenu ref="saveMenu"/>
+          <DownloadMenu ref="downloadMenu"/>
+          <RightClickMenu ref="rightClickMenu"/>
+          <SettingsMenu ref="settingsMenu"/>
+
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import VarGraph from "./elements/VarGraph"
+  import NavigationDrawer from "./elements/NavigationDrawer";
+  import GraphHeader from "./elements/GraphHeader";
+  import CreateControls from "./controls/CreateControls";
+  import DetailControls from "./controls/DetailControls";
+  import ZoomControls from "./controls/ZoomControls";
+  import NewGraphMenu from "./menus/NewGraphMenu";
+  import SaveMenu from "./menus/SaveMenu";
+  import DownloadMenu from "./menus/DownloadMenu";
+  import RightClickMenu from "./menus/RightClickMenu";
+  import SettingsMenu from "./menus/SettingsMenu";
+
+  export default {
+    name: "NodeControls",
+    components: {
+      VarGraph,
+      NavigationDrawer,
+      GraphHeader,
+      CreateControls,
+      DetailControls,
+      NewGraphMenu,
+      SaveMenu,
+      DownloadMenu,
+      RightClickMenu,
+      ZoomControls,
+      SettingsMenu
+    },
+    props: {
+      source: String,
+    },
+    methods: {
+      toggleDrawer() {
+        this.$refs.navigationDrawer.toggleDrawer();
+      }
+    }
+  }
+</script>
---
+++ removedcode/Designs/ToolBarDesign/components/NavigationDrawer.vue
@@ -0,0 +1,366 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :clipped="$vuetify.breakpoint.lgAndUp"
+    app
+    light
+  >
+
+<!--    <v-divider></v-divider>-->
+
+<!--    <v-row justify="center" align="center" class="mt-5">-->
+<!--      <v-btn rounded outlined color="primary">-->
+<!--        <v-icon class="mr-2">mdi-plus-circle-outline</v-icon>-->
+<!--        Neuer Zustand-->
+<!--      </v-btn>-->
+<!--    </v-row>-->
+<!--    <v-row justify="center" align="center" class="mt-5 mb-5">-->
+<!--      <v-btn rounded outlined color="primary">-->
+<!--        <v-icon class="mr-2">mdi-link-variant-plus</v-icon>-->
+<!--        Neue Verknüpfung-->
+<!--      </v-btn>-->
+<!--    </v-row>-->
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <template v-for="item in items">
+        <v-row
+          v-if="item.heading"
+          :key="item.heading"
+          align="center"
+        >
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-col>
+          <v-col
+            cols="6"
+            class="text-center"
+          >
+            <a
+              href="#!"
+              class="body-2 black--text"
+            >EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(child, i) in item.children"
+            :key="i"
+            link
+          >
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ child.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item
+          v-else
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <template v-for="item in nodeItems">
+        <v-row
+          v-if="item.heading"
+          :key="item.heading"
+          align="center"
+        >
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-col>
+          <v-col
+            cols="6"
+            class="text-center"
+          >
+            <a
+              href="#!"
+              class="body-2 black--text"
+            >EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(child, i) in item.children"
+            :key="i"
+            link
+          >
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ child.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item
+          v-else
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <template v-for="item in edgeItems">
+        <v-row
+          v-if="item.heading"
+          :key="item.heading"
+          align="center"
+        >
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-col>
+          <v-col
+            cols="6"
+            class="text-center"
+          >
+            <a
+              href="#!"
+              class="body-2 black--text"
+            >EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(child, i) in item.children"
+            :key="i"
+            link
+          >
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ child.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item
+          v-else
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <template v-for="item in optimizationItems">
+        <v-row
+          v-if="item.heading"
+          :key="item.heading"
+          align="center"
+        >
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-col>
+          <v-col
+            cols="6"
+            class="text-center"
+          >
+            <a
+              href="#!"
+              class="body-2 black--text"
+            >EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(child, i) in item.children"
+            :key="i"
+            link
+          >
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ child.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item
+          v-else
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+
+  </v-navigation-drawer>
+</template>
+<script>
+
+  export default {
+    name: "NodeControls",
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      items: [
+        {icon: 'mdi-plus', text: 'Neuer Graph'},
+        {icon: 'mdi-database', text: 'Graph-Datenbank'},
+        {icon: 'mdi-import', text: 'Graph importieren'},
+        {icon: 'mdi-export', text: 'Graph exportieren'},
+      ],
+      nodeItems: [
+        {
+          icon: 'mdi-chevron-up',
+          'icon-alt': 'mdi-chevron-down',
+          text: 'Zustände',
+          model: false,
+          children: [
+            {icon: 'mdi-label', text: 'Testzustand 1'},
+            {icon: 'mdi-label', text: 'Testzustand 2'},
+            {icon: 'mdi-plus', text: 'Neuer Zustand'},
+          ],
+        }
+      ],
+      edgeItems: [
+        {
+          icon: 'mdi-chevron-up',
+          'icon-alt': 'mdi-chevron-down',
+          text: 'Verknüpfungen',
+          model: false,
+          children: [
+            {icon: 'mdi-label', text: 'Testverknüpfung'},
+            {icon: 'mdi-plus', text: 'Neue Verknüpfung'},
+          ],
+        }
+      ],
+      optimizationItems: [
+        {
+          icon: 'mdi-chevron-up',
+          'icon-alt': 'mdi-chevron-down',
+          text: 'Optimale Wege',
+          model: false,
+          children: [
+            {icon: 'mdi-label', text: '512,24€ | 123122s'},
+            {icon: 'mdi-refresh', text: 'Optimierung starten'},
+          ],
+        }
+      ],
+    }),
+    methods: {
+      toggleDrawer() {
+        this.drawer = ! this.drawer;
+      }
+    }
+  }
+</script>
