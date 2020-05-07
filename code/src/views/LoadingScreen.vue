cef7a983fdbca9c4702ea98311b982d27f5033ef
Linus Herterich <l.herterich@confirado.de>
2020-05-07 12:16:04 +0200

    VG-177 UI Design Loading

--- code/src/views/LoadingScreen.vue
+++ code/src/views/LoadingScreen.vue
@@ -2,7 +2,10 @@
 
 <template>
   <div>
-    <h1>Loading...</h1>
+    <h1 class="ma-10 text-center" >
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </h1>
+    <p class="ma-10">Bitte warte einen Moment.</p>
   </div>
 </template>
 
@@ -12,13 +15,14 @@
 export default {
   name: "LoadingScreen",
   mounted: function() {
-    if (Date.now() > (this.$store.getters.getIssuedTime + 3000000000000)) {
-          localStorage.removeItem("store"); //To do: dont remove store, only user 
-          this.$store.commit("logout");
-          this.$router.replace("/home/login");
+    if (Date.now() > this.$store.getters.getIssuedTime + 3000000000000) {
+      localStorage.removeItem("store"); //To do: dont remove store, only user
+      this.$store.commit("logout");
+      this.$router.replace("/home/login");
     } else if (this.$store.getters.getAuth && window.history.length > 1) {
       this.$router.replace(this.$route.query.redirect);
-    } else if (this.$store.getters.getAuth && window.history.length <= 1) { //To do: check if graph exists in storage and redirect correctly
+    } else if (this.$store.getters.getAuth && window.history.length <= 1) {
+      //To do: check if graph exists in storage and redirect correctly
       this.$router.replace("/home/menu");
     } else {
       this.$router.replace("/home/login");
