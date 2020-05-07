52155f8f9226929f758d5d73ffe55b81ff427ba3
Linus Herterich <l.herterich@confirado.de>
2020-05-07 12:59:39 +0200

    Fixed Test

--- code/cypress/integration/Controls/NewGraphControls_spec.js
+++ code/cypress/integration/Controls/NewGraphControls_spec.js
@@ -34,6 +34,7 @@ describe("NewGraphControls", () => {
     //rerouting to create new graph
     expect(cy.route("http://localhost:8080/home/menu"));
     //get back to the main page
+    cy.login();
     cy.get("#newGraph").click();
     cy.get("#prodname").type("Testprodukt");
     cy.get("#prodquantity").type("1000");
@@ -65,6 +66,7 @@ describe("NewGraphControls", () => {
       return false;
     });
     //get back to the main page
+    cy.login();
     cy.get("#newGraph").click();
     cy.get("#prodname").type("Testprodukt");
     cy.get("#prodquantity").type("1000");
@@ -85,6 +87,7 @@ describe("NewGraphControls", () => {
       return false;
     });
     //get back to the main page
+    cy.login();
     cy.get("#newGraph").click();
     cy.get("#prodname").type("Testprodukt");
     cy.get("#prodquantity").type("1000");
