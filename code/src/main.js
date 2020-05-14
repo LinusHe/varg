6cedf235f215868cac0c8056ddf6e3ba112eb4fa
Jonas Gwozdz <jonas.gwozdz@stud.htwk-leipzig.de>
2020-05-14 14:33:30 +0200

    VG-186 darkmode options inserted

--- code/src/main.js
+++ code/src/main.js
@@ -14,7 +14,23 @@ import Darkmode from 'darkmode-js';
 Vue.config.productionTip = false;
 
 //Darkmode
-new Darkmode().showWidget();
+var options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#eee', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
 
 // Vue.use(VueCytoscape);
 
