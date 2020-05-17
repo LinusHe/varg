479189c1efe427722a4f5dde9e3f561473053d2b
Jonas Gwozdz <jonas.gwozdz@stud.htwk-leipzig.de>
2020-05-17 22:20:31 +0200

    VG-186 Toggle in Settings

--- code/src/main.js
+++ code/src/main.js
@@ -6,34 +6,13 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import Vuex from "vuex";
 import { store } from "./store/store.js";
-import Darkmode from 'darkmode-js';
 
 
 
 // import VueCytoscape from "vue-cytoscape";
 Vue.config.productionTip = false;
 
-//Darkmode
-var options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#eee', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
-
-
 // Vue.use(VueCytoscape);
-
 Vue.directive("click-outside", {
   bind: function(el, binding, vnode) {
     this.event = function(event) {
