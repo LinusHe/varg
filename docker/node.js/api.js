6f9345a18125537ae2fad03064b2d10e96e530a7
Parsus77 <julius.hohlfeld@gmail.com>
2020-06-18 09:46:51 +0200

    Extra comments

--- docker/node.js/api.js
+++ docker/node.js/api.js
@@ -1,19 +1,21 @@
 /* eslint-disable no-console */
 //Node.js setup for interface between front and backend
-//to test:
-//cd to code
-//node api.js
 
 //uses express module and mysqljs 
 const express = require('express');
 const mysql_driver = require('mysql');
 
+//config object holds information for database access
 const config = {
     // eheldt: 192.168.1.103
     // jhohlfel: 192.168.99.101
+    //host => adress of database server
     host: "192.168.99.101",
+    //user that accesses the database
     user: "varg",
+    //password to be used (has to match database password)
     password: "VarG2020",
+    //which database on the server to access
     database: "vargdb"
 };
 
@@ -22,8 +24,11 @@ const con=mysql_driver.createConnection(config);
 
 //Node connects to DB, test
 con.connect(function(err) {
+    //if an error occures during connection handle it here
     if (err){
+        //shows up in the server log
         console.log("Test: Connection failed!");
+        //rethrows error
         throw err;
     }
     console.log("Test: Connected!");
@@ -40,15 +45,16 @@ let router = express.Router();
 //middleware functionality (inspection, logging etc.) here
 router.use(function(req,res,next) {
     console.log("middleware could happen here");
-    //TODO: Serverside Verification could happen here with req.query.user 
+
     //this will allow to (only) access the resources from the specified address
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     next(); //continue past middleware
 });
 
-//test route
+//test route ('/' = root)
 router.get('/', (req, res) =>{
     res.json({message: 'api is responsive'});
 });
@@ -62,22 +68,29 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 //login
 router.route('/login?')
-    //get login data
+    //catch the login-Request
     .post(function (req, res) {
         console.log("Sending Log-In data.");
+        //access the information send in the request body
         let userName = req.body.user;
         let password = req.body.password;
         console.log(userName + ", " + password);
+        //universal password for all users - no specification of user
+        //if we had a user database, we could log them into their accounts here
         if (userName === "VarG" && password === "2020"){
             console.log(userName + ", willkommen in der Matrix.");
+            //account information
             let user = {
                 name: userName,
                 role: 'student',
                 issued: Date.now(),
+                //lets the client know that login was succesfull
                 authenticated: true
             }
+            //Sends user object back to client
             res.send(user);
         }
+        //error 403 = "forbidden"
         else res.sendStatus(403);
     });
 
@@ -87,6 +100,7 @@ router.route('/graph?')
     //get all graphs - should probably be reserved to higher authority roles
     .get(function (req, res) {
         console.log('Sending all Graphs');
+        //Queries the databse with SQL Query
         con.query("SELECT graphObject FROM cytographs", function(err, result) {
             if (err) throw err;
             console.log("Query was successful !");
@@ -96,14 +110,17 @@ router.route('/graph?')
     //post a graph
     .post(function(req,res) {
         console.log('Attempting to post a graph with filename:',req.body.filename);
+        //all information needed for creating a new entry, read from request body
         let post = {fileID: req.body.fileId,
                     filename: req.body.filename, 
                     userName: req.body.user, 
                     graphObject: req.body.json};
+        //query syntax makes safe string escapes, (protection against injections)
         con.query("INSERT INTO cytographs SET ?", post,
          function(err, result, fields) {
             if (err) throw err;
             console.log("Post was succesfull.");
+            //status 201 = "Succesful"
             res.sendStatus(201);
         });
     });
@@ -114,6 +131,7 @@ router.route('/graph/meta')
     .get(function (req, res) {
         console.log('Sending all metadata of user:',req.query.user);
         let userName= req.query.user;
+        //string escape syntax
         con.query('SELECT JSON_EXTRACT(graphObject,"$.data") AS "metadata", fileId, fileName, userName FROM cytographs WHERE userName = ?', [userName],
         function(err, result, fields) {
             if (err) throw err;
@@ -124,6 +142,10 @@ router.route('/graph/meta')
 
 //graph/:graph_id
 
+//everytime a call to the api is made with the structure /graph/:graph:id,
+//this code will be called.
+//It makes sure that the specified graph exists in the database
+
 router.param('graph_id', function(req, res, next, id)   {
     //check if Graph with id exists within database
     let userName=req.query.user;
@@ -131,11 +153,14 @@ router.param('graph_id', function(req, res, next, id)   {
     con.query("SELECT graphObject FROM cytographs WHERE fileID= ? AND userName = ?", [id, userName], 
     function(err, result, fields) {
         if (err) {
+            //connection issues may exist and the graph could still be in the db
             console.log("Graph may not exist.");
+            //route will not continue nad throw an error
             throw err;
         }
         else{
             console.log("Graph does exist.");
+            //next - route will continue normally
             next();
         }
     });
@@ -144,8 +169,7 @@ router.param('graph_id', function(req, res, next, id)   {
 router.route('/graph/:graph_id?')
     //get a single graph identified by id
     .get(function(req, res) {
-        //the query should involve some check if the user "owns" the graph
-        //example: SELECT graphObject FROM cytographs WHERE fileID=1 AND user=jdeo
+        //user can only access the graphs under his username
         let id=req.params.graph_id;
         let userName=req.query.user;
         console.log('Sending Graph with id:',id,', username:',userName);
@@ -166,10 +190,11 @@ router.route('/graph/:graph_id?')
         }
         con.query("UPDATE cytographs SET graphObject = ? WHERE fileID = ? AND userName = ?",
             [put.graphObject, put.fileID, put.userName],
-        function(err, result, fields) {
-            if (err) throw err;
-            console.log(result.affectedRows + " record(s) updated");
-            res.sendStatus(200);
+            function(err, result, fields) {
+                if (err) throw err;
+                //shows how many rows were changed
+                console.log(result.affectedRows + " record(s) updated");
+                res.sendStatus(200);
         });
     })
     //delete a single graph identified by id
@@ -213,6 +238,7 @@ api.use('/VarG', router);
 //START the server
 
 //8080 is the port number -> probably needs to change
+//on server: port 80 (HTTP), port 443 (HTTPS)
 api.listen(8080, () => {
     console.log('API listens to 8080');
 });
